adad421643619012ba195e82c00bf354f2091f6b
Becky Smith <becky.smith@thedatalab.org>
2021-07-27 14:29:38 +0100

    Split out osrelease and jobrunner_stats config loading

--- tests/test_jobrunner_stats.py
+++ tests/test_jobrunner_stats.py
@@ -10,7 +10,7 @@ from publisher import jobrunner_stats
 
 
 def test_successful_push_message(capsys, study_repo, temp_jobrunner):
-    jobrunner_stats.main(
+    extracted = jobrunner_stats.main(
         current_dir=os.getcwd(),
         job_runner_dir=temp_jobrunner.name,
         days_to_extract=7,
@@ -19,6 +19,7 @@ def test_successful_push_message(capsys, study_repo, temp_jobrunner):
         token="",
     )
 
-    captured = capsys.readouterr()
+    assert extracted
 
+    captured = capsys.readouterr()
     assert captured.out.startswith("Pushed new extraction file"), captured.out
